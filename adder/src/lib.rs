da8a17f154cf2ad2803cf220ef9ae4592295cc61
Manish <itzmanish108@gmail.com>
2021-12-18 16:59:09 +0530

    tests

---
+++ adder/src/lib.rs
@@ -0,0 +1,120 @@
+// #[derive(Debug)]
+// struct Rectangle {
+//     width: u32,
+//     height: u32,
+// }
+
+// impl Rectangle {
+//     fn can_hold(&self, other: &Rectangle) -> bool {
+//         self.width > other.width && self.height > other.height
+//     }
+// }
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+//     // #[test]
+//     // fn exploration() {
+//     //     let result = 2 + 2;
+//     //     assert_eq!(result, 4);
+//     // }
+
+//     // below test fails
+//     // #[test]
+//     // fn make_fail() {
+//     //     panic!("Make this test fail")
+//     // }
+
+//     #[test]
+//     fn larger_can_hold_smaller() {
+//         let larger = Rectangle {
+//             width: 8,
+//             height: 7,
+//         };
+//         let smaller = Rectangle {
+//             width: 5,
+//             height: 1,
+//         };
+
+//         assert!(larger.can_hold(&smaller));
+//     }
+
+//     #[test]
+//     fn smaller_cannot_hold_larger() {
+//         let larger = Rectangle {
+//             width: 8,
+//             height: 7,
+//         };
+//         let smaller = Rectangle {
+//             width: 5,
+//             height: 1,
+//         };
+
+//         assert!(!smaller.can_hold(&larger));
+//     }
+// }
+
+// should panic
+// pub struct Guess {
+//     value: i32,
+// }
+
+// impl Guess {
+//     pub fn new(value: i32) -> Guess {
+//         if value < 1 || value > 100 {
+//             panic!("Guess value must be between 1 and 100, got {}.", value);
+//         }
+
+//         Guess { value }
+//     }
+// }
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     #[should_panic(expected="some string")]
+//     fn greater_than_100() {
+//         Guess::new(200);
+//     }
+// }
+
+// Using Result<T,E>
+// #[cfg(test)]
+// mod tests {
+//     #[test]
+//     fn it_works() -> Result<(), String> {
+//         if 2 + 2 == 4 {
+//             Ok(())
+//         } else {
+//             Err(String::from("two plus two does not equal four"))
+//         }
+//     }
+
+//     // we can ignore a test with ignore annotation
+//     #[test]
+//     #[ignore]
+//     fn expensive_test() {
+//         // code that takes an hour to run
+//     }
+// }
+
+// testing internal fn and integration test
+pub fn add_two(a: i32) -> i32 {
+    internal_adder(a, 2)
+}
+
+fn internal_adder(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn internal() {
+        assert_eq!(4, internal_adder(2, 2));
+    }
+}
